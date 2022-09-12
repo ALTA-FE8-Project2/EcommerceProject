8496ec3dac2d3bbf1e33f2a4e52d1103e8808dbe
herbayulet <sayaherbayu@gmail.com>
2022-09-12 20:59:49 +0700

    update container on layouting

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const History = () => {
-    return <div>History</div>;
+    return (
+        <div>
+            <div className="row">
+                <div className="col-12 sm-col-6 md-col-4 lg-col-4 xl-col-4"></div>
+            </div>
+        </div>
+    );
 };
 
 export default History;
