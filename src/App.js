f8b0b3dff59174c4b3e19165f948dde7ae64c401
herbayulet <sayaherbayu@gmail.com>
2022-09-12 19:05:14 +0700

    Checkout order done but not responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Checkout } from "./pages/Checkout";
+import History from "./pages/History";
 
 function App() {
     return (
         <>
-            <Checkout />
+            <Router>
+                <Routes>
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/history" element={<History />} />
+                </Routes>
+            </Router>
         </>
     );
 }
