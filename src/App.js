59485bce802a1aaebf84b413709e525518c2804b
herbayulet <sayaherbayu@gmail.com>
2022-09-12 18:38:32 +0700

    new update Checkout

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Checkout from "./pages/Checkout";
+import { Checkout } from "./pages/Checkout";
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,56 @@
 import React from "react";
-
-const Checkout = () => {
-  return (
-    <>
-      <h1>Checkout</h1>
-    </>
-  );
+export const Checkout = () => {
+    return (
+        <>
+            <div className="w-full h-[684px] bg-glass">
+                <div className="container mx-auto">
+                    <h1 className="text-3xl font-poppins">Checkout Items</h1>
+                    <hr className="mt-2 border-black" />
+                    <div className="h-auto mt-5 rounded-l">
+                        <div class="grid grid-rows-3 grid-flow-col gap-4">
+                            <div class="row-span-3 col-span-3 bg-main py-10 rounded-lg">
+                                <div className="grid grid-cols-2 gap-4">
+                                    <div className="ml-4">
+                                        <h1 className="text-2xl">Items from E-Boox</h1>
+                                        <h3 className="mt-8 text-xl">Items</h3>
+                                        <div className="flex mt-5">
+                                            <img src="#" alt="#" />
+                                            <p className="ml-8 font-thin">Karyakarsa1</p>
+                                        </div>
+                                    </div>
+                                    <div className="items-end">
+                                        <h1 className="text-2xl">Date: 1 Januari 2022</h1>
+                                        <h3 className="mt-8 text-xl">Price</h3>
+                                        <div className="flex mt-5">
+                                            <p className="font-thin">Rp 25.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-span-2 row-span-2 bg-main py-10 rounded-lg">
+                                <div className="ml-4">
+                                    <h1 className="text-2xl">Order Summary:</h1>
+                                    <h3 className="mt-5 text-xl">Subtotal</h3>
+                                    <p className="text-base font-thin">Rp 25.000</p>
+                                    <div className="flex justify-between">
+                                        <h3 className="mt-5 text-xl">Shipping</h3>
+                                        <p className="mt-4 mr-3 text-base font-thin">Rp 0</p>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h3 className="mt-5 text-xl">Estimated Tax</h3>
+                                        <p className="mt-4 mr-3 text-base font-thin">Rp 0</p>
+                                    </div>
+                                    <div className="flex justify-between">
+                                        <h3 className="mt-5 text-xl">Total</h3>
+                                        <p className="mt-4 mr-3 text-base font-thin">Rp 25.000</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-span-2 bg-main py-10">03</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
-
-export default Checkout;
