ba333e6768bb3c0127a168ca92c802ea20a5174b
herbayulet <sayaherbayu@gmail.com>
2022-09-12 14:01:02 +0700

    tets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./App.css";
+// import Checkout from "./pages/Checkout";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
+    return <>{/* <Checkout /> */}</>;
 }
 
 export default App;
