9c4311c0294088668cc02a729fb350f0d73f6d70
rizaalsyt <rizalsyt017@gamil.com>
2022-09-13 17:24:26 +0700

    featurHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,139 @@
-import React from "react";
+  import React from "react";
 
-const Home = () => {
-  return (
-    <>
-      <h1>Home Page</h1>
-    </>
-  );
-};
+  const Home = () => {
+    return (
+      <>
+        <section className=" my-24 max-w-screen-xl mx-auto">
+          <div className=" grid grid-flow-col  gap-8 my-24 py-1">
+            <div className="flex flex-col   py-20 my-3 col-span-6 mb-10">
+            <h1 className=" font-roboto font-bold text-5xl text-limeEboox">e-Boox
+            </h1>
+            <h1 className=" font-roboto font-bold text-5xl mt-2">The Best <span className="text-limeEboox">Online</span>
+            </h1>
+            <h1 className=" font-roboto font-bold text-5xl mt-2">Book
+            <span className="text-limeEboox"> Store</span>
+            </h1>
+            <div>
+            <button className="bg-cyanEboox flex justify-between  text-white active:bg-limeEboox-300 font-bold text-sm mt-16 py-3 px-6 rounded-md shadow hover:lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150   " >
+            Get Our It  <span> <img src="https://cdn.discordapp.com/attachments/1017789562757591111/1019105238650736660/undraw_handcrafts_arrow_1.png" alt="" className="h-6 ml-auto flex justify-center"  /> </span>  
+            </button>
+            </div>
+            </div>
+            <div className="relative col-span-7 ">
+              <img src="https://cdn.discordapp.com/attachments/1017789562757591111/1019099877348757505/ilustrator.png" alt="" className="absolute h-96 mx-48" />
+          </div>
+          </div>
+          </section>
+          
+          
+          {/* product */}
 
-export default Home;
+          <section className=" bg-main min-h-screen">
+            <div className="p-10 text-center font-bold mb-10">
+              <h1 className="text-5xl font-roboto md:text3xl ">Product</h1>
+            </div>
+            <div className="flex flex-wrap justify-center mt-10 ">
+              
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+              <div className="border-black flex flex-col-3 bg-white rounded-md shadow-md  m-6  sm:w-auto ">
+                <div>
+                  <img src=" https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt="" className="h-44 -mt-8 m-6 " />
+                </div>
+                <div className="flex flex-col">
+                <h1 className=" mt-5  text-lg font-poppins">Garis Waktu</h1> 
+                  <h2 className=" text-lg text-gray-400 font-poppins"> Author : Fiersa Bersari</h2>
+                  <h2 className=" text-lg  font-poppins m-1">Rp. 150.000</h2>
+                  <button className="bg-cyanEboox rounded-md text-white text-center hover:bg-lime-700 transition-all duration-500"> Add To Cart</button>
+                </div>
+               
+              </div>
+            </div>
+          </section>
+
+
+
+          {/* <section className="bg-main min-h-screen my-12">
+            <h1 className="text-4xl p-10 text-black font-bold font-roboto text-center">Product</h1>
+            <div className=" border container mx-auto flex flex-col-4"> 
+              <div className=" border grid grid-cols-3">
+              <div className="border flex flex-wrap px-28">
+                  <div className=" border-blue relative flex flex-col-3 bg-white rounded-lg shadow-md  ">
+                    <div className="">
+                      <img src="https://cdn.discordapp.com/attachments/1017709537312116772/1019144286027128882/gariswaktu.png" alt=""  className="h-44 -mt-8 m-6 "/>
+                    </div>
+                    <h1 className="ml-10">Garis Waktu</h1>
+                  </div>
+              </div>
+              </div>
+              </div>
+
+      
+
+
+          </section> */}
+          
+        
+      </>
+    );
+  };
+
+  export default Home;
