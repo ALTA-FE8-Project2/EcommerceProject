278d205f226c7be09c5118c2b4a20458f8f27577
herbayulet <sayaherbayu@gmail.com>
2022-09-14 12:39:11 +0700

    darkmode done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import History from "./pages/History";
 import Home from "./pages/Home";
@@ -9,30 +10,43 @@ import Details from "./pages/Details";
 
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import { ThemeContext } from "./utils/DarkmodeContext";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/history-order" element={<History />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route
-          path="*"
-          element={
-            <div className="grid content-center justify-center min-h-screen text-2xl font-bold sm:text-5xl">
-              <span className="inline h-8 "> 404 Error Not Found........</span>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    const [theme, setTheme] = useState("light");
+    const background = useMemo(() => ({ theme, setTheme }), [theme]);
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme]);
+    return (
+        <ThemeContext.Provider value={background}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/details" element={<Details />} />
+                    <Route path="/history-order" element={<History />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route
+                        path="*"
+                        element={
+                            <div className="grid content-center justify-center min-h-screen text-2xl font-bold sm:text-5xl">
+                                <span className="inline h-8 "> 404 Error Not Found........</span>
+                            </div>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </ThemeContext.Provider>
+    );
 }
 
 export default App;
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const History = () => {
     return (
-        <>
-            <div className="w-full min-h-screen bg-main font-poppins">
+        <div className=" bg-glass dark:bg-slate-800">
+            <Navbar />
+            <div className="w-full min-h-screen font-poppins ">
                 <div className="container mx-auto lg:px-9 xl:px-0 md:px-9">
-                    <h1 className="pt-10 text-3xl fonts-poppins">Order History</h1>
-                    <hr className="mt-2 border-black" />
+                    <h1 className="pt-10 text-3xl fonts-poppins text-slate-700 dark:text-white">Order History</h1>
+                    <hr className="mt-2 border-slate-700 dark:border-white" />
                     <div className="h-auto py-10 mt-5 bg-white rounded-xl">
                         <div className="px-10">
                             <div className="flex justify-between">
-                                <h1>
-                                    Date: 1 Januari 2022 <span className="ml-3">Order: #123456789</span>
+                                <h1 className="text-slate-700">
+                                    Date: 1 Januari 2022 <span className="ml-3 text-slate-700">Order: #123456789</span>
                                 </h1>
                                 <div className="grid">
-                                    <h1>Total</h1>
-                                    <p className="mt-3">Rp 500.000</p>
+                                    <h1 className="text-slate-700">Total</h1>
+                                    <p className="mt-3 text-slate-700">Rp 500.000</p>
                                 </div>
                             </div>
                             <div className="flex justify-between mt-3">
@@ -25,10 +27,10 @@ const History = () => {
                                         alt="#"
                                         className="w-10 h-10"
                                     />
-                                    <p className="ml-8 text-right font-base">Karyakarsa1</p>
+                                    <p className="ml-8 text-right font-base text-slate-700">Karyakarsa1</p>
                                 </div>
                                 <div className="flex mt-5">
-                                    <h3 className="text-limeEboox ">
+                                    <h3 className="cursor-pointer text-limeEboox">
                                         Detail Transactions <span className="px-3 py-3 ml-5 text-white rounded-xl bg-cyanEboox">Buy Again</span>
                                     </h3>
                                 </div>
@@ -40,21 +42,21 @@ const History = () => {
                     <div className="h-auto py-10 mt-5 bg-white rounded-xl">
                         <div className="px-10">
                             <div className="flex justify-between">
-                                <h1>
-                                    Date: 1 Januari 2022 <span className="ml-3">Order: #123456789</span>
+                                <h1 className="text-slate-700">
+                                    Date: 2 Januari 2022 <span className="ml-3">Order: #123456789</span>
                                 </h1>
                                 <div className="grid">
-                                    <h1>Total</h1>
-                                    <p className="mt-3">Rp 500.000</p>
+                                    <h1 className="text-slate-700">Total</h1>
+                                    <p className="mt-3 text-slate-700">Rp 500.000</p>
                                 </div>
                             </div>
                             <div className="flex justify-between mt-3">
                                 <div className="flex items-center mt-3">
                                     <img src="https://matematika.uinsu.ac.id/wp-content/uploads/2021/02/book.jpeg" alt="#" className="w-10 h-10" />
-                                    <p className="ml-8 text-right font-base">Karyakarsa1</p>
+                                    <p className="ml-8 text-right font-base text-slate-700">Karyakarsa1</p>
                                 </div>
                                 <div className="flex mt-5">
-                                    <h3 className="text-limeEboox ">
+                                    <h3 className="cursor-pointer text-limeEboox">
                                         Detail Transactions <span className="px-3 py-3 ml-5 text-white rounded-xl bg-cyanEboox">Buy Again</span>
                                     </h3>
                                 </div>
@@ -63,7 +65,7 @@ const History = () => {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
